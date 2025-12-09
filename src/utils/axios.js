6205ea5254997d8e52a03a9d65cabd28a3822703
nas2dev <nas2dev@gmail.com>
2025-12-09 22:25:50 +0100

    wip

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { mirrorRequest } from './refreshToken';
 import { useUserStore } from "@/stores/useUserStore"
 const Axios = axios.create({
-    baseURL: import.meta.env.NODE_ENV === "production" ?
+    baseURL: import.meta.env.PROD === "production" ?
         'https://lm-prod-api.codingtrainingacademy.com/api' :
         'http://127.0.0.1:8000/api',
     timeout: 10000,
